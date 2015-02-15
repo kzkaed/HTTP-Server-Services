c9fddf55e66d68dcc5766351cb23155958c806fd
kzkaed <kzkaeding@gmail.com>
2015-02-15 15:41:05 -0600

    server Main and Server separated

---
+++ src/server/Main.java
@@ -0,0 +1,23 @@
+package server;
+
+import java.net.ServerSocket;
+import java.util.Map;
+
+import server.socket.WireServerSocketWrapper;
+
+
+public class Main {
+
+public static void main(String[] args) throws Exception{
+		
+		ArgsParser parser = new ArgsParser();
+		Map<String, String> context = parser.parse(args);
+		String portString = context.get("Port");
+		String publicDirectory = context.get("Public Directory");
+		int port = Integer.parseInt(portString);
+		
+		ServerSocket serverSocket = new ServerSocket(port);
+		new Server(new WireServerSocketWrapper(serverSocket),port,publicDirectory).start();
+	}
+
+}
---
+++ test/server/ServerTest.java
@@ -0,0 +1,92 @@
+package server;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.Before;
+
+import server.Server;
+import server.socket.ServerSocketService;
+import server.socket.WireServerSocketWrapper;
+import server.mocks.MockServerSocketWrapper;
+import server.mocks.MockServerSocket;
+import server.mocks.MockSocket;
+
+
+public class ServerTest {
+	
+	ServerSocketService service;
+	int port;
+	String document;
+	MockServerSocket mockSSocket;
+	
+	
+	@Before
+	public void setUp() throws IOException {
+		port = 5000;
+		document = "PUBLIC_DIR";
+		mockSSocket = new MockServerSocket(port);
+	}
+
+	@After
+	public void tearDown() throws IOException {
+		try {
+			if(mockSSocket !=null ){
+				mockSSocket.close();
+				
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	@Test
+	public void testServerConstructed() throws Exception {
+		Server server = new Server(mockSSocket,port,document);
+		assertEquals(document, "PUBLIC_DIR");
+		assertEquals(port, 5000);
+		assertEquals(mockSSocket.getClass().toString(), "class server.mocks.MockServerSocket");
+	}
+	
+	@Test
+	public void testServerStart() throws Exception {
+		ServerSocket serverSocket = new ServerSocket(1234);
+		service = new WireServerSocketWrapper(serverSocket);
+		Server server = new Server(service,port,document);
+		ServerStarter starter = new ServerStarter(server);
+		starter.start();
+		
+		serverSocket.close();
+		assertEquals(service.isClosed(),true);
+		assertEquals(service.getClass().getName(),"server.socket.WireServerSocketWrapper");
+		
+	}
+	
+	
+	
+	class ServerStarter extends Thread {
+        public Server server;
+        
+        public ServerStarter(Server mServer) {
+            this.server = mServer;
+        }
+
+        public void run() {
+        	try {
+				server.start();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+    
+        }
+            
+    }
+		
+}
