a5dd193a6a1d3ee19ee86d58ae592d8b80da020e
kzkaed <kzkaeding@gmail.com>
2015-03-16 15:16:14 -0500

    remove unused imports

--- test/server/constants/MethodTest.java
+++ test/server/constants/MethodTest.java
@@ -1,24 +1,13 @@
 package server.constants;
 
 import static org.junit.Assert.*;
-
 import java.io.IOException;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-
 import server.constants.Method;
-import server.request.Request;
-import server.response.ResponseBuilder;
+
 
 public class MethodTest {
 	
-	@Before
-	public void setUp() throws Exception {
-		
-	}
-
 	@Test
 	public void testGET() {
 		assertEquals(Method.GET,"GET");
